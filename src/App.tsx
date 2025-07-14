058695ba987dc85fe463a45376eadfad5c1c374c
Ruby Chen <rc071404@bu.edu>
2025-07-13 21:31:56 -0400

    set up template for app.tsx and component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import {useEffect, useState} from "react";
+import styled from "styled-components";
 
-function App() {
+export default function App(){
+
+  const [ , ] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const rawData = await fetch();
+      const data = await rawData.json();
+      setData(data);
+    }
+    fetchData()
+        .then(() => console.log("Data fetched successfully"))
+        .catch((e: Error) => console.log("There was the error: " + e));
+  }, );
 
   return (
     <>
 
     </>
-  )
-}
+  );
 
-export default App
+}
